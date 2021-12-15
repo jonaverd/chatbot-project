0ea9c4baf2d268f506abdd0b9e735b10ac1f5091
Jonathan Verdu <jonaverd95@gmail.com>
2021-12-15 23:05:43 +0100

    Actualizar rutas del proyecto
    Actualizar las rutas de los scripts del proyecto para coincidir con la carpeta de GitHub.

--- Project/Scripts/public.ps1
+++ Project/Scripts/public.ps1
@@ -4,7 +4,7 @@ Read-Host -Prompt "tunneling server ngrok..."
 
 # Conecta con el servidor publico nGrok (Tunneling de localhost)
 Invoke-Expression 'cmd /c start powershell -Command { 
-    & "D:\Cosas Personales\Trabajo Final\Proyecto\Project\Scripts\Executables\ngrok.exe" http 8080 ; 
+    & "D:\Cosas Personales\Trabajo Final\Proyecto\chatbot-project\Project\Scripts\Executables\ngrok.exe" http 8080 ; 
     Read-Host -Prompt "public host opened..." 
 }'
 
